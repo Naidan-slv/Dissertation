5e05ae1afff69f4965f882aaf81797c4ff55afbb
Naidan <naidan.salvador2@hotmail.com>
2026-03-02 12:40:48 +0000

    feat: mesh skeleton code

---
+++ src/meshes/mesh.py
@@ -0,0 +1,12 @@
+class Mesh:
+    def vertices(self):
+        pass
+
+    def neighbors(self, v):
+        pass
+
+    def value(self, v):
+        pass
+
+    def sorted_vertices(self):
+        pass
