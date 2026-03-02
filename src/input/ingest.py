10a774e13ed1903389203c487f90b1780e0812f9
Naidan <naidan.salvador2@hotmail.com>
2026-03-02 12:06:26 +0000

    chore : yaml file configurations for raw files and ingest.py reads accordingly

--- src/input/ingest.py
+++ src/input/ingest.py
@@ -1,6 +1,31 @@
 import yaml
+import numpy as np
+from pathlib import Path
 
-with open("datasets.yaml", "r") as f:
-    config = yaml.safe_load(f)
+THIS_DIR = Path(__file__).resolve().parent
 
-print(config)
+# Path to datasets.yaml
+YAML_PATH = THIS_DIR / "datasets.yaml"
+
+with open(YAML_PATH, "r") as f:
+    cfg = yaml.safe_load(f)
+
+meta = cfg["datasets"]["aneurism"]
+
+print(meta)
+
+w, h, d = meta["shape_whd"]
+print("Shape (w,h,d):", w, h, d)
+
+PROJECT_ROOT = THIS_DIR.parent.parent
+
+# Construct absolute path to raw file
+raw_path = PROJECT_ROOT / meta["path"]
+
+print("Resolved path:", raw_path)
+
+data = np.fromfile(raw_path, dtype=meta["dtype"])
+
+vol = data.reshape((d, h, w), order="C")
+
+print("Min:", vol.min(), "Max:", vol.max())
