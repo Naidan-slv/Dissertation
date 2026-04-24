94292d8c80828fa14e487101d02eb3e44c994b25
Naidan-Slv <naidan.salvador2@hotmail.com>
2026-04-24 19:57:44 +0100

    feat : implement _walk_to_saddle

--- src/contour_tree_algo/persistence.py
+++ src/contour_tree_algo/persistence.py
@@ -17,4 +17,12 @@ def compute_persistence_pairs(supernodes, superarcs, value_fn):
 
 def _walk_to_saddle(leaf, adj):
     """Walk from leaf along superarcs until reaching a saddle (degree >= 3)."""
-    raise NotImplementedError
+    prev, curr = None, leaf
+    while True:
+        neighbours = adj[curr]
+        if len(neighbours) >= 3:
+            return curr
+        if len(neighbours) == 1 and curr != leaf:
+            return curr
+        nxt = next(n for n in neighbours if n != prev)
+        prev, curr = curr, nxt
