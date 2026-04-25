a97e2287221d68bda0d0e86a95d11c4d9b2c7a2a
Naidan-Slv <naidan.salvador2@hotmail.com>
2026-04-25 20:43:03 +0100

    test : add single triangle marching tet cases

--- tests/isosurface/test_marching_tet.py
+++ tests/isosurface/test_marching_tet.py
@@ -2,7 +2,9 @@
 Tests for marching tetrahedra.
 """
 
-from src.isosurface.marching_tet import tetrahedron_triangles
+import pytest
+
+from src.isosurface.marching_tet import interpolate_edge, tetrahedron_triangles
 
 
 class TestTetrahedronEmptyCases:
@@ -19,3 +21,21 @@ class TestTetrahedronEmptyCases:
         values = [0.6, 0.7, 0.8, 0.9]
 
         assert tetrahedron_triangles(points, values, 0.5) == []
+
+
+class TestTetrahedronSingleTriangle:
+    """One vertex on one side of the isovalue gives one triangle."""
+
+    def test_midpoint_iso_yields_geometric_midpoint(self):
+        point = interpolate_edge((0, 0, 0), (1, 0, 0), 0.0, 1.0, 0.5)
+
+        assert point == pytest.approx((0.5, 0.0, 0.0))
+
+    def test_single_vertex_above_produces_one_triangle(self):
+        points = [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)]
+        values = [1.0, 0.0, 0.0, 0.0]
+
+        triangles = tetrahedron_triangles(points, values, 0.5)
+
+        assert len(triangles) == 1
+        assert len(triangles[0]) == 3
