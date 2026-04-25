e8d844d138c8895dece735292ecd304ab870e3ef
Naidan-Slv <naidan.salvador2@hotmail.com>
2026-04-25 20:41:15 +0100

    feat : marching tetrahedra skeleton

---
+++ src/isosurface/marching_tet.py
@@ -0,0 +1,21 @@
+"""
+Marching tetrahedra isosurface extraction.
+
+Refs: Topological Manipulation of Isosurfaces, Ch 5;
+      Simplicial Subdivisions and Sampling Artifacts.
+"""
+
+
+def tetrahedron_triangles(points, values, isovalue):
+    """Return triangles cut from one tetrahedron."""
+    raise NotImplementedError
+
+
+def interpolate_edge(p0, p1, v0, v1, isovalue):
+    """Linearly interpolate one isosurface edge crossing."""
+    raise NotImplementedError
+
+
+def extract_isosurface(width, height, depth, data, isovalue):
+    """Extract vertices and triangles from a regular grid."""
+    raise NotImplementedError
